058fa91e5b27d24c43a5874759cd901e220b3319
Joseph <wangj11@uw.edu>
2018-05-22 17:16:07 -0700

    Reformat Data

--- scripts/set_up.R
+++ scripts/set_up.R
@@ -2,3 +2,8 @@ library(dplyr)
 
 
 hate_crimes <- read.csv("data/hate_crimes.csv", stringsAsFactors = F)
+
+hate_crimes_formatted <- hate_crimes %>%
+  select(state, median_household_income, gini_index,
+         share_population_with_high_school_degree, share_unemployed_seasonal,
+         hate_crimes_per_100k_splc)
