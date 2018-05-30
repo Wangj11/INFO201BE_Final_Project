2c08506ee62300fe5438bd555868e8cb4cc0ec07
Joseph <wangj11@uw.edu>
2018-05-30 12:59:05 -0700

    edited about blurb and linted

--- ui.R
+++ ui.R
@@ -8,32 +8,44 @@ source("scripts/build_map.R")
 
 shinyUI(navbarPage(tags$h1("Hate Crimes Data"),
   theme = shinytheme("sandstone"),
-  #Tab 1
+  # Tab 1
   tabPanel(
     tags$h2("About"),
     tags$link(rel = "stylesheet", type = "text/css", href = "project.css"),
-    sidebarLayout(position = "left",
-      sidebarPanel(tags$h3("About the Team"),
-                   tags$h3("Raymond Huang", tags$br(),
-                           "Sarah Liu", tags$br(),
-                           "Joseph Wang", tags$br(),
-                           "Yu-won Youn")),
-      mainPanel(tags$p("This site allows users to observe the correlations
+    sidebarLayout(
+      position = "left",
+      sidebarPanel(
+        tags$h3("About the Team"),
+        tags$h3(
+          "Raymond Huang", tags$br(),
+          "Sarah Liu", tags$br(),
+          "Joseph Wang", tags$br(),
+          "Yu-won Youn"
+        )
+      ),
+      mainPanel(tags$p(
+        "Compared to other countries around the world, the US
+                       sees a large number of hate crimes where people are
+                       discriminated against for their identities. This site
+                       allows users to observe the correlations
                        between income inequality and hate crimes. The data used
                        in this site is a collection of data from a variety of
                        sources including the Census Bureau, FBI, Southern
                        Poverty Law Center, Kaiser Family Foundation, and more.
                        The dataset includes information about median household
                        income, gini index,and various information about the
-                       status of inhabitants by state as well as the number of 
+                       status of inhabitants by state as well as the number of
                        hate crimes.",
-                tags$br(), "This site shows the relationship hate crimes and these
+        tags$br(), "This site shows the relationship hate crimes and these
                        various statistics in different ways to see where the
                        focus should be when trying to lower hate crimes. The
                        maps and charts allow for comparison between the various
-                       information.")))),
-  
-  #Tab 2
+                       information."
+      ))
+    )
+  ),
+
+  # Tab 2
   tabPanel(
     tags$h2("Maps"),
     sidebarLayout(
@@ -45,31 +57,41 @@ shinyUI(navbarPage(tags$h1("Hate Crimes Data"),
             "Gini Index" = "gini_index",
             "Population with HS Degree" =
               "share_population_with_high_school_degree",
-            "Seasonal Unemployed" = "share_unemployed_seasonal"),
-          selected = "Median Household Income"),
+            "Seasonal Unemployed" = "share_unemployed_seasonal"
+          ),
+          selected = "Median Household Income"
+        ),
         tags$p("The top map shows the number of hate crimes for every 100k
                people while the map below shows the selected information,
-               allowing for quick comparisons between the two.")),
-      mainPanel(plotlyOutput("static_map"), plotlyOutput("by_state")))),
-  
-  #Tab 3
+               allowing for quick comparisons between the two.")
+      ),
+      mainPanel(plotlyOutput("static_map"), plotlyOutput("by_state"))
+    )
+  ),
+
+  # Tab 3
   tabPanel(
     tags$h2("Correlation Charts"),
     sidebarLayout(
       sidebarPanel(
         selectInput("charts",
-                    label = "Correlate data with:",
-                    choices = list( 
-                      "Median Household Income" = "median_household_income", 
-                      "Gini Index" = "gini_index", 
-                      "Population with HS Degree"  = 
-                        "share_population_with_high_school_degree", 
-                      "Seasonal Unemployed"= "share_unemployed_seasonal"),
-                    selected = "Median Household Income"),
+          label = "Correlate data with:",
+          choices = list(
+            "Median Household Income" = "median_household_income",
+            "Gini Index" = "gini_index",
+            "Population with HS Degree" =
+              "share_population_with_high_school_degree",
+            "Seasonal Unemployed" = "share_unemployed_seasonal"
+          ),
+          selected = "Median Household Income"
+        ),
         tags$p("The chart created shows the relationship between hate crimes and
                the selected information on a scatter plot, allowing for quick
-               visualization of any correlation between the two.")),
+               visualization of any correlation between the two.")
+      ),
       mainPanel(
         plotlyOutput("scatter")
-      )))
-)) #navbarPage
+      )
+    )
+  )
+)) # navbarPage
